3f8a8ce9a123298dec1359678535f0b27d148043
Bruna Grassi <101432594+BrunaGrassi@users.noreply.github.com>
2024-09-26 17:32:49 -0300

    v2
    adicionando métodos construtores e pra retornar valores das variáveis

--- Carro.java
+++ Carro.java
@@ -8,6 +8,15 @@ public class Carro {
 
     private boolean acelerando = false;
 
+    public Carro(String modelo, String motor, String placa ) {
+        this.modelo = modelo;
+        this.motor = motor;
+        this.placa = placa;
+    }
+
+    public Carro() {
+
+    }
 
     public void acelerar() {
         acelerando = true;
@@ -29,6 +38,15 @@ public class Carro {
 
     public void ligarLuzFreio() {
         System.out.println("Luzes de freio ligadas");
+    }
+
+    public void getInformacoes() {
+        System.out.println("Informações sobre o veículo:");
+        System.out.println("Modelo: " + getModelo());
+        System.out.println("Motor: " + getMotor());
+        System.out.println("Quantidade Portas: " + getQtdPortas());
+        System.out.println("Placa: " + getPlaca());
+        System.out.println("Cor: " + getCor());
     }
 
     public String getModelo() {
--- PricipalCarro.java
+++ PricipalCarro.java
@@ -10,13 +10,7 @@ public class PricipalCarro {
         obj.setPlaca("JIU-3520");
         obj.setCor("Preto");
 
-        System.out.println("Informações sobre o veículo:");
-        System.out.println("Modelo: " + obj.getModelo());
-        System.out.println("Motor: " + obj.getMotor());
-        System.out.println("Quantidade Portas: " + obj.getQtdPortas());
-        System.out.println("Placa: " + obj.getPlaca());
-        System.out.println("Cor: " + obj.getCor());
-
+        obj.getInformacoes();
         obj.acelerar();
         obj.parar();
 
